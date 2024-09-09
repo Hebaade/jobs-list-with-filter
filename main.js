49c4319ca23f23ad347f4bf54c5c65de2e5d4467
heba <hebaadelali78@gmail.com>
2024-09-09 19:36:58 +0300

    commit

--- main.js
+++ main.js
@@ -2,8 +2,17 @@ let search=document.getElementById("search");
 let items=document.querySelectorAll(".item")
 let list=document.querySelector(".list")
 let clear=document.getElementById("Clear")
+let words=document.querySelectorAll(".features span")
 let newItems=[]
-
+for (let i = 0; i < words.length; i++) {
+    words[i].addEventListener("click",()=> {
+        search.classList.remove("d-none")
+        search.value=words[i].innerHTML
+        filterItems()
+        clear.classList.toggle("d-none")
+    })
+    
+}
 function filterItems() {
 for (let i = 0; i < items.length; i++) {
   if (items[i].innerHTML.toLowerCase().includes(search.value.toLowerCase())) {
